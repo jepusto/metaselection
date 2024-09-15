acfc6e0453294b45da763daaf998ae3916eee155
jepusto <jepusto@gmail.com>
2024-09-15 14:54:09 -0500

    Tweaked exponentiate option in print.selmodel() to also exponentiate the CI bounds and to use a delta-method approximation for the standard error. Also added an option to exponentiate the zeta parameters.

--- R/print.selmodel.R
+++ R/print.selmodel.R
@@ -4,23 +4,38 @@
 #' 
 #' 
 #' @param x Fitted model of class \code{"selmodel"}.
-#' @param exponentiate logical with `TRUE` indicating that the gamma and zeta estimates should be exponentiated
+#' @param transf_gamma logical with `TRUE` indicating that the heterogeneity parameter estimates (called gamma) should be transformed by exponentiating.
+#' @param transf_zeta logical with `TRUE` indicating that the selection parameter estimates (called zeta) should be transformed by exponentiating.
 #' @param ... Not used.
 #'
 #' @export
 
 
 
-print.selmodel <- function(x, exponentiate = TRUE, ...) {
+print.selmodel <- function(x, transf_gamma = FALSE, transf_zeta = FALSE, ...) {
   
   model <- if ("step.selmodel" %in% class(x)) "Step Function Model with Robust Variance Estimation"
   
   estimates <- x$est
   
-  if (exponentiate) {
-    
-    estimates$Est[estimates$param == "gamma"] <- exp(estimates$Est)[estimates$param == "gamma"] 
-    
+  if (transf_gamma | transf_zeta) {
+    transf_variables <- intersect(
+      names(estimates),
+      c("Est","CI_lo","CI_hi","percentile_lower","percentile_upper","basic_lower","basic_upper","student_lower","student_upper")
+    )
+  }
+  if (transf_gamma) {
+    gamma_params <- grepl("^gamma", estimates$param)
+    estimates[gamma_params, transf_variables] <- exp(estimates[gamma_params,transf_variables])
+    estimates$SE[gamma_params] <- estimates$Est[gamma_params] * estimates$SE[gamma_params]
+    estimates$param <- sub("^gamma","tau2", estimates$param)
+  }
+  
+  if (transf_zeta) {
+    zeta_params <- grepl("^zeta", estimates$param)
+    estimates[zeta_params, transf_variables] <- exp(estimates[zeta_params, transf_variables])
+    estimates$SE[zeta_params] <- estimates$Est[zeta_params] * estimates$SE[zeta_params]
+    estimates$param <- sub("^zeta","lambda_", estimates$param)
   }
   
   call <- x$cl
