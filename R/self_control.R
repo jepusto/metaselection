0dc88a5bb91574f5053d8c29e01eec5a9a328ad5
Megha Joshi <33493716+meghapsimatrix@users.noreply.github.com>
2024-09-16 15:18:51 -0500

    write documentation for the data

--- R/self_control.R
+++ R/self_control.R
@@ -13,9 +13,24 @@
 #'   \item{se_g}{standard error of the effect size}
 #'   \item{outcome}{name of the outcome measure}
 #'   \item{comparison}{conditions compared}
-#'   \item{type_of_treatment}{type of treatment}
+#'   \item{type_of_treatment}{type of treatment/training}
+#'   \item{type_of_treatment}{length of treatment coded in days}
+#'   \item{publication_status}{indicator for whether the study was published}
+#'   \item{publication_status_new}{indicator for whether the study was published}
+#'   \item{publication_year}{year that the study was published}
+#'   \item{research_group}{indicator for whether researchers of the study belonged to the strength model group}
+#'   \item{control_group_quality}{quality of the control group with active indicating that the control group worked on some task}
+#'   \item{gender_ratio}{percentage of males in the sample}
+#'   \item{type_of_outcome}{type of outcome}
+#'   \item{subjectivity_of_outcome_measurement}{indicator for the subjectivity of the outcome measure}
+#'   \item{lab_based_versus_real_world_behavior}{indicator for whether the behavior measured was assessed in lab or in the real-world}
+#'   \item{stamina_versus_strength}{indicator for whether the outcome was assessed with (Stamina) or without a preceding effortful task (Strength)}
+#'   \item{pre_test_measure}{indicator for thether there was a pre and post measure of outcome or only post measure}
 #'   \item{sample_population}{population examined in the study}
 #'   \item{sample_age}{average age of the sample}
+#'   \item{attrition}{percentage of attrition}
+#'   \item{partipant_compensation}{type of compensation received by the participants}
+#'   \item{self_control_potential}{indicator for whether the outcome measure required utilization of full self-control potential}
 #' }
 #'
 #'
