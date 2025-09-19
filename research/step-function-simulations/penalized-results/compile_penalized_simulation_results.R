19ee5e5bebee629bbebb1f7d94ead798dd7450a7
jepusto <jepusto@gmail.com>
2025-09-19 08:29:29 -0500

    Makin' graphs.

--- research/step-function-simulations/penalized-results/compile_penalized_simulation_results.R
+++ research/step-function-simulations/penalized-results/compile_penalized_simulation_results.R
@@ -83,10 +83,10 @@ res %>%
 
 write_rds(res, file = "research/step-function-simulations/penalized-results/sim-step-function-penalized-results.rds", compress = "gz", compression = 9L)
 
+
 res %>%
   filter(param == "beta") %>%
   arrange(mean_smd, tau, omega, cor_mu, weights, m, n_multiplier, estimator, priors) %>%
-  select(mean_smd, tau, omega, cor_mu, weights, m, n_multiplier, estimator, priors, bias, rmse)
 
 
 res_beta <- 
@@ -115,4 +115,87 @@ ggplot(res_beta) +
     y = "RMSE",
     color = "Estimator",
     fill = "Estimator"
+  )
+
+res_beta_wide <- 
+  res_beta %>%
+  select(mean_smd, tau, omega, cor_mu, weights, m, n_multiplier, estimator, priors, bias, rmse) %>%
+  unite("estimator_prior", estimator, priors) %>%
+  pivot_wider(names_from = estimator_prior, values_from = c(bias, rmse))
+
+res_beta_wide %>%
+  ggplot() + 
+  aes(bias_CML_None, bias_CML_Default, color = factor(m)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  facet_grid(mean_smd ~ tau, scales = "free") + 
+  theme_light() + 
+  labs(
+    x = "Flat", 
+    y = "Default",
+    color = "Sample size"
+  )
+
+res_beta_wide %>%
+  ggplot() + 
+  aes(rmse_CML_None, rmse_CML_Default, color = factor(m)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  facet_grid(mean_smd ~ tau, scales = "free") + 
+  theme_light() + 
+  labs(
+    x = "Flat", 
+    y = "Default",
+    color = "Sample size"
+  )
+
+
+res_beta_wide %>%
+  ggplot() + 
+  aes(bias_ARGL_None, bias_ARGL_Default, color = factor(m)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  facet_grid(mean_smd ~ tau, scales = "free") + 
+  theme_light() + 
+  labs(
+    x = "Flat", 
+    y = "Default",
+    color = "Sample size"
+  )
+
+
+res_beta_wide %>%
+  ggplot() + 
+  aes(rmse_ARGL_None, rmse_ARGL_Default, color = factor(m)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  facet_grid(mean_smd ~ tau, scales = "free") + 
+  theme_light() + 
+  labs(
+    x = "Flat", 
+    y = "Default",
+    color = "Sample size"
+  )
+
+res_beta_wide %>%
+  ggplot() + 
+  aes(rmse_CML_Default, rmse_ARGL_Default, color = factor(m)) + 
+  geom_vline(xintercept = 0) + 
+  geom_hline(yintercept = 0) + 
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
+  geom_point() + 
+  facet_grid(mean_smd ~ tau, scales = "free") + 
+  theme_light() + 
+  labs(
+    x = "CML", 
+    y = "ARGL",
+    color = "Sample size"
   )
